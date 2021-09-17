f40bde716ad4e3d03b27d33e1d7dd6ff644457a3
Morgan <morgan.blem@gmail.com>
2021-09-17 15:13:11 +0200

    Fixed a big in the test bench which caused improper reinitialisation

--- test_model.py
+++ test_model.py
@@ -22,31 +22,88 @@ component = 0  # 0 to predict trend, 1 to predict duration, 2 for a dual approac
 
 # hyperparameters #
 hidden_size = 64                                                                                            # x
-lr = 0.001                                                                                                  # x
+lr = 0.01                                                                                                  # x
 batch_size = 64                                                                                             # x
-seq_length = 4                                                                                              # x
-dropout = 0.1                                                                                               # x
+seq_length = 8                                                                                              # x
+dropout = 0.2                                                                                               # x
 training_epochs = 500                                                                                       # x
 # TCN only:
-kernel_size = 4                                                                                             # x
-n_layers = 3                                                                                                # x
+kernel_size = 2                                                                                             # x
+n_layers = 4                                                                                                # x
 
 trends = preprocess(dataset[0], dataset[1], dataset[2])
 
 
 # now just simply uncomment the model you'd like to test:
 
-model = MLP(seq_length*2, hidden_size, max(1,component), dropout).to(dev)
-#model = CNN(seq_length, hidden_size, max(1,component), 2, dropout).to(dev)
-#model = TCN(seq_length,max(1, component), [hidden_size]*n_layers, kernel_size, dropout).to(dev)
-#model = LSTM(seq_length, hidden_size, max(1,component), dropout).to(dev)
-#model = RNN(max(1,component), 2, hidden_size, 1, dropout).to(dev)
-#model = LSTM(max(1,component), 2, hidden_size, 1, dropout).to(dev)
-#model = BiLSTM(max(1,component), 2, hidden_size, 1, dropout).to(dev)
+def create_DNN():                                                                                           # x
+    #return MLP(seq_length*2, hidden_size, max(1,component), dropout).to(dev)
+    #return CNN(seq_length, hidden_size, max(1,component), 2, dropout).to(dev)
+    return TCN(seq_length,max(1, component), [hidden_size]*n_layers, kernel_size, dropout).to(dev)
+    #return LSTM(seq_length, hidden_size, max(1,component), dropout).to(dev)
+    #return RNN(max(1,component), 2, hidden_size, 1, dropout).to(dev)
+    #return LSTM(max(1,component), 2, hidden_size, 1, dropout).to(dev)
+    #return BiLSTM(max(1,component), 2, hidden_size, 1, dropout).to(dev)
 
+outputfile = "" # if this is empty it will just print instead
 
-train_and_test(model, trends, train_proportion, models_to_average, lr, batch_size, seq_length, training_epochs, component)
+print_output = True if outputfile == "" else False
+if not print_output:
+    outf = open(outputfile, 'a')
+import math
+import statistics
+res_1 = []
+res_2 = []
+tp = 0 # true positives
+tn = 0 # true negatives
+fp = 0 # false positives
+fn = 0 # false negatives
 
+for x in range(models_to_average):
+    model = create_DNN() # create a fresh model
+    result = train_and_test(model, trends, train_proportion, lr, batch_size, seq_length, training_epochs, component) # train and test it
+
+    if component == 2:
+        res_1.append(math.sqrt(result[0]))
+        res_2.append(math.sqrt(result[1]))
+        if print_output: print(f'{math.sqrt(result[0])}, {math.sqrt(result[1])}')
+        classification = result[2::]
+    else:
+        res_1.append(math.sqrt(result[0]))
+        if print_output: print(f'{math.sqrt(result[0])}')
+        classification = result[1::]
+    tp += classification[0]
+    tn += classification[1]
+    fp += classification[2]
+    fn += classification[3]
+    os.remove("temp.pt")
+
+if component == 0 or component == 2:
+    if print_output: print("slope results:") 
+    else: outf.write("slope results:\n")
+    
+    if print_output: print(f'μ = {round(sum(res_1) / len(res_1 ),3)} | σ = {round(statistics.pstdev(res_1),3)} | tp = {tp} | tn = {tn} | fp = {fp} | fn = {fn}')
+    else: outf.write(f'av = {round(sum(res_1) / len(res_1 ),3)} | dev = {round(statistics.pstdev(res_1),3)} | tp = {tp} | tn = {tn} | fp = {fp} | fn = {fn}\n')
+    sensitivity = tp/(tp+fn)
+    specificity = tn/(tn+fp)
+    f1 = 2*sensitivity*specificity/(sensitivity+specificity)
+    if print_output: print(f'accuracy = {(tp+tn)/(fp+fn+tp+tn)} | sensitivity = {sensitivity} | specificity = {specificity} | F1 = {f1}')
+    else: outf.write(f'accuracy = {(tp+tn)/(fp+fn+tp+tn)} | sensitivity = {sensitivity} | specificity = {specificity} | F1 = {f1}\n')
+
+if component == 1:
+    if print_output: print(f'μ = {round(sum(res_1) / len(res_1 ),3)} | σ = {round(statistics.pstdev(res_1),3)}')
+    else: 
+        outf.write("length results:\n")
+        outf.write(f'av = {round(sum(res_1) / len(res_1 ),3)} | dev = {round(statistics.pstdev(res_1),3)}\n')
+
+if component == 2:
+    if print_output: print(f'μ = {round(sum(res_2) / len(res_2 ),3)} | σ = {round(statistics.pstdev(res_2),3)}')
+    else:
+        outf.write("length results:\n")
+        outf.write(f'av = {round(sum(res_2) / len(res_2 ),3)} | dev = {round(statistics.pstdev(res_2),3)}\n')
+if not print_output:
+    outf.write("\n")
+    outf.close()
 
 
 
--- utils/dnn_methods.py
+++ utils/dnn_methods.py
@@ -3,6 +3,8 @@ import torch.nn.functional as F
 from torch.autograd import Variable
 import numpy as np
 import models
+import os
+import copy
 
 if torch.cuda.is_available():  
     dev = torch.device("cuda:0")
@@ -126,197 +128,168 @@ def dataload(window_func ,batch_size, data, seq_len, train_proportion, component
 
 
 
-def test_model(model, trainset, validateset, testset, learning_rate, component, training_epochs, models_to_average, outputfile):
-    outf = open(outputfile, 'a')
-    import math
-    import statistics
-    res_1 = []
-    res_2 = []
-    fp = 0 # false positives
-    tp = 0 # true positives
-    fn = 0 # false negatives
-    tn = 0 # true negatives
-    total_dir_acc = 0
-    for i in range(models_to_average):
-        epochs = training_epochs
-        import torch.optim as optim
-        train_loss = []
-        validation_loss = []
+def test_model(model, trainset, validateset, testset, learning_rate, component, training_epochs):
+    tp = 0
+    fp = 0
+    tn = 0
+    fn = 0
+    epochs = training_epochs
+    import torch.optim as optim
+    train_loss = []
+    validation_loss = []
+
+    epoch_total_trainloss = 0 # the total loss for each epoch, used for plotting
+    min_val_loss_epoch = 0 # the epoch with the lowest validation loss
+    min_val_loss = 9999999 # the lowest validation loss
+    optimizer = optim.Adam(model.parameters(), lr=learning_rate)
+    validation_direction_accuracy = []
+
+    for epoch in range(epochs+1):
+
+        epoch_total_trainloss = 0 # reset this for the validation epoch'''
         model.train()
-        epoch_total_trainloss = 0 # the total loss for each epoch, used for plotting
-        min_val_loss_epoch = 0 # the epoch with the lowest validation loss
-        min_val_loss = 9999999 # the lowest validation loss
-        optimizer = optim.Adam(model.parameters(), lr=learning_rate)
-        validation_direction_accuracy = []
+        for data in trainset:  # for each batch
+            features, labels = data  # split the batches up into their features and labels
+            model.zero_grad()
+            output = model(features) # get a prediction from the model
+            loss = F.mse_loss(output, labels)  # calculate the loss of our prediction
+            loss.backward()  # backpropogate the loss
+            optimizer.step()  # optimize weights
+            epoch_total_trainloss += loss.item()/len(trainset)
+            torch.cuda.synchronize()
+        train_loss.append(epoch_total_trainloss) # add this epoch's loss in order to plot it later
+        epoch_total_trainloss = 0 # reset this for the validation epoch
 
-        for epoch in range(epochs+1):
-
-            epoch_total_trainloss = 0 # reset this for the validation epoch'''
-            model.train()
-            for data in trainset:  # for each batch
-                features, labels = data  # split the batches up into their features and labels
-                model.zero_grad()
-                output = model(features) # get a prediction from the model
-                loss = F.mse_loss(output, labels)  # calculate the loss of our prediction
-                loss.backward()  # backpropogate the loss
-                optimizer.step()  # optimize weights
-                epoch_total_trainloss += loss.item()/len(trainset)
-                torch.cuda.synchronize()
-            train_loss.append(epoch_total_trainloss) # add this epoch's loss in order to plot it later
-            epoch_total_trainloss = 0 # reset this for the validation epoch
-
-            # now we'll calculate the direction accuracy for the training and validation sets
-            correct=0
-            total_points = 0
-            model.eval()
-            for data in validateset:
-                
-                inputs, labels = data
-                output = model(inputs)
-                total_points += len(output)
-                for i in range(len(output)):
-                    pred = output[i]
-                    actual = labels[i]
-                    #if pred < 0 and actual < 0 or pred > 0 and actual > 0: #or (pred-actual)<0.01:
-                    #    correct += 1
-                    #print(output[0],labels[0])
-                loss = F.mse_loss(output, labels)  # calculate the loss of our prediction
-                epoch_total_trainloss += loss.item()/len(validateset)
-                torch.cuda.synchronize()
-            if epoch_total_trainloss < min_val_loss:
-                torch.save(model.state_dict(), 'temp.pt')
-                min_val_loss = epoch_total_trainloss
-                min_val_loss_epoch = epoch
-            validation_direction_accuracy.append(correct/(total_points))
-            validation_loss.append(epoch_total_trainloss) # we'll need to plot validation loss too
-        #import matplotlib.pyplot as plt
-        #plt.plot(train_loss)
-        #plt.plot(validation_loss)
-        #plt.show()
-        #plt.plot(validation_direction_accuracy)
-        #plt.show()
-        #print(f"Lowest validation loss: {min_val_loss} at epoch {min_val_loss_epoch}")
-
-        model.load_state_dict(torch.load('temp.pt'))
-        model.eval()
+        # now we'll calculate the direction accuracy for the training and validation sets
         correct=0
-        output_file = open("utils/angles.txt", "w")
-        for data in trainset:
-
+        total_points = 0
+        model.eval()
+        for data in validateset:
+            
             inputs, labels = data
             output = model(inputs)
+            total_points += len(output)
             for i in range(len(output)):
                 pred = output[i]
-                #output_file.write(str(pred.item()*90)+"\n")
-        total_loss = 0
-        for data in validateset:
-            inputs, labels = data
-            output = model(inputs)
+                actual = labels[i]
+                #if pred < 0 and actual < 0 or pred > 0 and actual > 0: #or (pred-actual)<0.01:
+                #    correct += 1
+                #print(output[0],labels[0])
+            loss = F.mse_loss(output, labels)  # calculate the loss of our prediction
+            epoch_total_trainloss += loss.item()/len(validateset)
+            torch.cuda.synchronize()
+        if epoch_total_trainloss < min_val_loss:
+            torch.save(model.state_dict(), 'temp.pt')
+            min_val_loss = epoch_total_trainloss
+            min_val_loss_epoch = epoch
+        validation_direction_accuracy.append(correct/(total_points))
+        validation_loss.append(epoch_total_trainloss) # we'll need to plot validation loss too
+    #import matplotlib.pyplot as plt
+    #plt.plot(train_loss)
+    #plt.plot(validation_loss)
+    #plt.show()
+    #plt.plot(validation_direction_accuracy)
+    #plt.show()
+    #print(f"Lowest validation loss: {min_val_loss} at epoch {min_val_loss_epoch}")
+
+    model.load_state_dict(torch.load('temp.pt'))
+
+    model.eval()
+    correct=0
+    output_file = open("utils/angles.txt", "w")
+    for data in trainset:
+
+        inputs, labels = data
+        output = model(inputs)
+        for i in range(len(output)):
+            pred = output[i]
+            #output_file.write(str(pred.item()*90)+"\n")
+    total_loss = 0
+    for data in validateset:
+        inputs, labels = data
+        output = model(inputs)
+        model.zero_grad()
+        total_loss += F.mse_loss(output, labels).item()/len(validateset)
+    #print(f'Directional Accuracy: {correct*100/len(test)} MSE on validate set: {total_loss}')
+
+    total_loss = 0
+    total_loss_slope = 0
+    total_loss_length = 0
+
+    for data in testset:
+        inputs, labels = data
+        output = model(inputs)
+
+        # test for the dual model
+        if component == 2:
+            output_slopes = []
+            for out in labels:
+                output_slopes.append(np.array([out[0]]))
+            output_slopes = Variable(torch.FloatTensor(output_slopes))
+
+            output_lengths = []
+            for out in labels:
+                output_lengths.append(np.array([out[1]]))
+            output_lengths = Variable(torch.FloatTensor(output_lengths))
+
+            pred_slopes = []
+            for out in output:
+                pred_slopes.append(np.array([out[0]]))
+            pred_slopes = Variable(torch.FloatTensor(pred_slopes))
+
+            pred_lengths = []
+            for out in output:
+                pred_lengths.append(np.array([out[1]]))
+            pred_lengths = Variable(torch.FloatTensor(pred_lengths))
+        
+            for i in range(len(output)): # true and false directional classifications
+                pred = output[i][0]
+                actual = labels[i][0]
+                if pred > 0 and actual > 0 or (pred-actual)<0.022: # true positive with 2 degree lee way
+                    tp += 1
+                elif pred < 0 and actual < 0: # true negative
+                    tn += 1
+                elif pred > 0 and actual < 0: # false positive
+                    fp += 1
+                elif pred < 0 and actual > 0: # false negative
+                    fn += 1
+
             model.zero_grad()
-            total_loss += F.mse_loss(output, labels).item()/len(validateset)
-        #print(f'Directional Accuracy: {correct*100/len(test)} MSE on validate set: {total_loss}')
-
-        total_loss = 0
-        total_loss_slope = 0
-        total_loss_length = 0
-
-        for data in testset:
-            inputs, labels = data
-            output = model(inputs)
-
-            # test for the dual model
-            if component == 2:
-                output_slopes = []
-                for out in labels:
-                    output_slopes.append(np.array([out[0]]))
-                output_slopes = Variable(torch.FloatTensor(output_slopes))
-
-                output_lengths = []
-                for out in labels:
-                    output_lengths.append(np.array([out[1]]))
-                output_lengths = Variable(torch.FloatTensor(output_lengths))
-
-                pred_slopes = []
-                for out in output:
-                    pred_slopes.append(np.array([out[0]]))
-                pred_slopes = Variable(torch.FloatTensor(pred_slopes))
-
-                pred_lengths = []
-                for out in output:
-                    pred_lengths.append(np.array([out[1]]))
-                pred_lengths = Variable(torch.FloatTensor(pred_lengths))
+            total_loss_slope += F.mse_loss(output_slopes,pred_slopes).item()/len(testset)
+            model.zero_grad()
+            total_loss_length += F.mse_loss(output_lengths, pred_lengths).item()/len(testset)
             
-                for i in range(len(output)): # true and false directional classifications
-                    pred = output[i][0]
-                    actual = labels[i][0]
-                    if pred > 0 and actual > 0 or (pred-actual)<0.022: # true positive with 2 degree lee way
-                        tp += 1
-                    elif pred < 0 and actual < 0: # true negative
-                        tn += 1
-                    elif pred > 0 and actual < 0: # false positive
-                        fp += 1
-                    elif pred < 0 and actual > 0: # false negative
-                        fn += 1
-
-                model.zero_grad()
-                total_loss_slope += F.mse_loss(output_slopes,pred_slopes).item()/len(testset)
-                model.zero_grad()
-                total_loss_length += F.mse_loss(output_lengths, pred_lengths).item()/len(testset)
+        
+        # test for single model
+        else:
+            model.zero_grad()
+            total_loss += F.mse_loss(output, labels).item()/len(testset)
+            for i in range(len(output)): # directional accuracy check
+                pred = output[i][0]
+                actual = labels[i][0]
+                if pred > 0 and actual > 0: # true positive with 2 degree lee way
+                    tp += 1
+                elif pred < 0 and actual < 0: # true negative
+                    tn += 1
+                elif pred > 0 and actual < 0: # false positive
+                    fp += 1
+                elif pred < 0 and actual > 0: # false negative
+                    fn += 1
             
-            # test for single model
-            else:
-                model.zero_grad()
-                total_loss += F.mse_loss(output, labels).item()/len(testset)
-                for i in range(len(output)): # directional accuracy check
-                    pred = output[i][0]
-                    actual = labels[i][0]
-                    if pred > 0 and actual > 0: # true positive with 2 degree lee way
-                        tp += 1
-                    elif pred < 0 and actual < 0: # true negative
-                        tn += 1
-                    elif pred > 0 and actual < 0: # false positive
-                        fp += 1
-                    elif pred < 0 and actual > 0: # false negative
-                        fn += 1
                         
-
+    if component == 2:
+        return [total_loss_slope, total_loss_length,tp,tn,fp,fn]
+    else:
+        return [total_loss,tp,tn,fp,fn]
         #print(f'Directional Accuracy: {correct*100/len(test)} MSE test: {total_loss}, RMSE test: {math.sqrt(total_loss)}\n')
         #print(f'{math.sqrt(total_loss_slope)}, {math.sqrt(total_loss_length)}')
         #testing_file.write(f'{math.sqrt(total_loss_slope)},{math.sqrt(total_loss_length)}\n')
 
-        if component == 2:
-            res_1.append(math.sqrt(total_loss_slope))
-            res_2.append(math.sqrt(total_loss_length))
-            #print(f'{math.sqrt(total_loss_slope)}, {math.sqrt(total_loss_length)}')
-        else:
-            res_1.append(math.sqrt(total_loss))
-            #print(f'{math.sqrt(total_loss)}')
     
-    if component == 0 or component == 2:
-        #print("slope results:")
-        outf.write("slope results:\n")
-        #print(f'μ = {round(sum(res_1) / len(res_1 ),3)} | σ = {round(statistics.pstdev(res_1),3)} | tp = {tp} | tn = {tn} | fp = {fp} | fn = {fn}')
-        outf.write(f'av = {round(sum(res_1) / len(res_1 ),3)} | dev = {round(statistics.pstdev(res_1),3)} | tp = {tp} | tn = {tn} | fp = {fp} | fn = {fn}\n')
-        sensitivity = tp/(tp+fn)
-        specificity = tn/(tn+fp)
-        f1 = 2*sensitivity*specificity/(sensitivity+specificity)
-        #print(f'accuracy = {(tp+tn)/(fp+fn+tp+tn)} | sensitivity = {sensitivity} | specificity = {specificity} | F1 = {f1}')
-        outf.write(f'accuracy = {(tp+tn)/(fp+fn+tp+tn)} | sensitivity = {sensitivity} | specificity = {specificity} | F1 = {f1}\n')
-
-    if component == 1:
-        #print(f'μ = {round(sum(res_1) / len(res_1 ),3)} | σ = {round(statistics.pstdev(res_1),3)}')
-        outf.write("length results:\n")
-        outf.write(f'av = {round(sum(res_1) / len(res_1 ),3)} | dev = {round(statistics.pstdev(res_1),3)}\n')
-    
-    if component == 2:
-        #print(f'μ = {round(sum(res_2) / len(res_2 ),3)} | σ = {round(statistics.pstdev(res_2),3)}')
-        outf.write("length results:\n")
-        outf.write(f'av = {round(sum(res_2) / len(res_2 ),3)} | dev = {round(statistics.pstdev(res_2),3)}\n')
-    outf.write("\n")
-    outf.close()
 
 
-def train_and_test(model, trends, train_proportion, models_to_average, lr, batch_size, seq_length, training_epochs, component, output_file):
+
+def train_and_test(model, trends, train_proportion, lr, batch_size, seq_length, training_epochs, component):
     s_window = sliding_window_MLP
 
     if isinstance(model, models.CNN) or isinstance(model, models.TCN):
@@ -324,4 +297,4 @@ def train_and_test(model, trends, train_proportion, models_to_average, lr, batch
     elif isinstance(model, models.RNN) or isinstance(model, models.LSTM) or isinstance(model, models.BiLSTM):
         s_window = sliding_window_RNN
     trainset, validationset, testset = dataload(s_window ,batch_size, trends, seq_length, train_proportion, component)
-    test_model(model, trainset, validationset, testset, lr, component, training_epochs, models_to_average, output_file)
+    return test_model(model, trainset, validationset, testset, lr, component, training_epochs)
